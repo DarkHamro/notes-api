e053928dac1f6237509923d55b055b247ee812d7
Amir <a29863869@gmail.com>
2026-04-28 22:36:04 +0500

    fix cors

--- notes-api/server.js
+++ notes-api/server.js
@@ -38,3 +38,9 @@ app.delete('/notes/:id', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
