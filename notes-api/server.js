d4ec004ec3358bdf714c7196625b1a42056fa35b
Amir <a29863869@gmail.com>
2026-04-28 23:09:43 +0500

    fix cors

--- notes-api/server.js
+++ notes-api/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import Database from 'better-sqlite3';
 import cors from "cors"
+app.use(cors());
 
 const app = express();
 const db = new Database('data.db');
@@ -25,6 +26,20 @@ app.post('/notes', (req, res) => {
   res.status(201).json({ id: result.lastInsertRowid, text });
 });
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+
+   if (req.method === "OPTIONS") {
+    return res.sendStatus(200)
+  }
+
+  next();
+});
+
 // READ — получить все заметки
 app.get('/notes', (req, res) => {
   const notes = db.prepare('SELECT * FROM notes').all();
@@ -40,18 +55,5 @@ app.delete('/notes/:id', (req, res) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-   if (req.method === "OPTIONS") {
-    return res.sendStatus(200)
-  }
-
-  next();
-});
-
-app.use(cors());
